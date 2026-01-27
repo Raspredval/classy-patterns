97f45081850e823f0a67a577f14ea2a6d37caa65
Raspredval <rustKravets@gmail.com>
2026-01-27 10:12:49 +0700

    more tests, captures are working fine, need to stress-test the capture performance tho, can be suboptimal

--- source/tests.cpp
+++ source/tests.cpp
@@ -4,34 +4,29 @@
 
 int main() {
     patt::Pattern
-        ptName  = patt::Capt(patt::Alpha() % 3) >> patt::Space() % 1 >> patt::Capt(patt::Alpha() % 3);
+        ptID        = (patt::Alpha() |= patt::Str("_")) >> (patt::Alnum() |= patt::Str("_")) % 0,
+        ptMacroName = patt::Str("$") >> patt::Capt(ptID) >> (patt::Str(".") >> patt::Capt(ptID)) % 0 >> patt::None();
+
     io::IOBufferStream
         buff;
-
-    io::cout.put("enter name: ");
     io::TextIO(buff)
         .forward_line_from(io::std_input)
         .go_start();
 
     patt::CaptureList
         captures;
-    auto
-        optMatch = patt::Eval(ptName, buff, captures);
-    if (!optMatch) {
-        io::cerr.put("failed to parse the name\n");
+    patt::OptMatch
+        optm        = patt::Eval(ptMacroName, buff, captures);
+    if (!optm) {
+        io::cerr.put("failed to parse macro name\n");
         return EXIT_FAILURE;
     }
 
-    if (captures.size() != 2) {
-        io::cerr.put("failed to capture parts of the name\n");
-        return EXIT_FAILURE;
+    for (const patt::Match& cpt : captures) {
+        std::string
+            strNamePart = cpt.GetString(buff);
+        io::cout.put(strNamePart).put_endl();
     }
-    
-    std::string
-        strFirstName    = captures[0].GetString(buff),
-        strSecondName   = captures[1].GetString(buff);
-    io::cout.fmt("parsed first name:  \"{}\"\n", strFirstName);
-    io::cout.fmt("parsed second name: \"{}\"\n", strSecondName);
 
     return EXIT_SUCCESS;
 }

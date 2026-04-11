a9e7b41b607c8e343adce14df322fad15d89da84
Raspredval <rustKravets@gmail.com>
2026-04-11 13:27:20 +0700

    renamed the tests.cpp file and updated the readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # classy-patterns
 PEG-like text parsing library, heavily inspired by lpeg
 
-example implementation of a JSON parser can be found in source/tests.cpp
+example implementation of a JSON parser can be found in tests/json.cpp
--- tests/json.cpp
+++ tests/json.cpp
@@ -6,9 +6,9 @@ static const patt::Grammar
     gramJSON    =
         std::invoke([] -> patt::Grammar {
             patt::Grammar g;
-            
+
             g["spacing"]    = patt::SpaceOrNewLine() % 0;
-            
+
             g["object"]     = patt::Str("{") >> g["spacing"] >> (
                 g["field"] >> g["spacing"] >>
                 (patt::Str(",") >> g["spacing"] >> g["field"] >> g["spacing"]) % 0
@@ -25,7 +25,7 @@ static const patt::Grammar
                 g["string"] | g["number"] |
                 g["object"] | g["array"] |
                 g["boolean"] | patt::Str("null");
-            
+
             g["boolean"]    = patt::Str("true") | patt::Str("false");
             g["string"]     = patt::Str("\"") >>
                 g["strfill"] >> (g["escseq"] >> g["strfill"]) % 0 >>
@@ -34,12 +34,12 @@ static const patt::Grammar
             g["escseq"]     = patt::Str("\\") >> (
                 patt::Set("/\"\\bfnrt") | (patt::Str("u") >> patt::HexDigit() * 4)
             );
-            
+
             g["number"]     = g["numint"] >> (patt::Str(".") >> g["numfract"]) % -1;
             g["numint"]     = patt::Str("-") % -1 >> patt::Digit() % 1;
             g["numfract"]   = patt::Digit() % 1 >> (patt::Set("eE") >> g["numexp"]) % -1;
             g["numexp"]     = (patt::Set("+-") % -1) >> patt::Digit() % 1;
-            
+
             g["__eval"]     = ((g["value"] % -1) >> patt::None()) /
                 [] (io::IStream& istream, const patt::OptMatch& optm, patt::CaptureList&, const std::any&) {
                     if (!optm)
@@ -56,10 +56,10 @@ main() {
     try {
         io::IFileStream
             ifileTestJSON   = io::IFileStream("./assets/test.json");
-    
+
         patt::OptMatch
             optmJSON        = patt::Eval(ifileTestJSON, gramJSON);
-        
+
         return ((bool)optmJSON)
             ? EXIT_SUCCESS : EXIT_FAILURE;
     }
